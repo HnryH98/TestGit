1a46bc903dbe5fbee28888f5db4f1f32cc5af53c
HnryH98 <73616079+HnryH98@users.noreply.github.com>
2020-10-30 21:09:12 +0000

    Update Member.java

--- Source Packages/layeredAdvisorgui/Member.java
+++ Source Packages/layeredAdvisorgui/Member.java
@@ -1,1 +1,62 @@
-a
+package layeredAdvisorgui;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hunth4
+ */
+public class Member {
+
+	private int ref;
+	private String firstName;
+        private String surname;
+        private String phone;
+        private String memType;
+
+	public Member() {
+	}
+
+	public int getRef() {
+		return ref;
+	}
+        
+        public void setRef(int aRef) {
+		ref = aRef;
+	}
+        
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String aFirstName) {
+		firstName = aFirstName;
+	}
+
+        public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String aSurname) {
+		surname = aSurname;
+	}
+        
+        public String getPhone() {
+                return surname;
+	}
+	public void setPhone(String aPhone) {
+		phone = aPhone;
+	}       
+	public String getMemType() {
+		return memType;
+	}
+        
+        public void setMemType(String aMemType) {
+		memType = aMemType;
+	}
+
+
+
+}
